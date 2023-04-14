53509aece28bdc41605cd71f8f8c2cdbb36ad3e5
lilchix <104845593+lilchix@users.noreply.github.com>
2023-04-14 14:46:55 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,35 @@
+function aggiungiAlCarrello() {
+  var carrello = document.getElementById("carrello");
+  var numero = parseInt(carrello.innerHTML);
+  numero++;
+  carrello.innerHTML = numero;
+}
+function rimuoviDalCarrello() {
+  var carrello = document.getElementById("carrello");
+  var numero = parseInt(carrello.innerHTML);
+  numero--;
+  carrello.innerHTML = numero;
+}
+function svuotaCarrello() {
+  var carrello = document.getElementById("carrello");
+  carrello.innerHTML = 0;
+}
+
+
+/*swiper*/
 var swiper = new Swiper(".mySwiper", {
-      effect: "coverflow",
-      grabCursor: true,
+      spaceBetween: 30,
       centeredSlides: true,
-      slidesPerView: "auto",
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
       },
       pagination: {
         el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
     });
-
-
