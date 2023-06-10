4a965e1e82913b8fa7592728b7b43c618a1fd0c2
lilchix <104845593+lilchix@users.noreply.github.com>
2023-06-10 16:05:29 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -15,51 +15,6 @@ function svuotaCarrello() {
   carrello.innerHTML = 0;
 }
 
-
-
-var Menu = {
-  el: {
-    menu: $('.menu'),
-    menuTop: $('.menu-top'),
-    menuClose: $('.menu-close'),
-    menuMiddle: $('.menu-middle'),
-    menuBottom: $('.menu-bottom'),
-    menuText: $('.menu-text')
-  },
-  
-  init: function() {
-    Menu.bindUIactions();
-  },
-  
-  bindUIactions: function() {
-    Menu.el.menu
-        .on(
-          'click',
-        function(event) {
-        Menu.activateMenu(event);
-        event.preventDefault();
-      }
-    );
-  },
-  
-  activateMenu: function() {
-    Menu.el.menuTop.toggleClass('menu-top-expand expand');
-    Menu.el.menuMiddle.toggleClass('menu-middle-expand expand');
-    Menu.el.menuBottom.toggleClass('menu-bottom-expand expand'); 
-    Menu.el.menuText.toggleClass('menu-text-expand');
-    Menu.el.menuClose.toggleClass('menu-close-visible');
-  }
-};
-  
-  //Stop menu item click closing the menu
-  $(".menu .menu-global").click(function(e) {
-      e.stopPropagation();
-});
-
-Menu.init();
-
-
-
 /*swiper*/
 var swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
@@ -76,7 +31,7 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
-    },)
+    }};
 
 /*jQuery ready compatto*/
     $(function
