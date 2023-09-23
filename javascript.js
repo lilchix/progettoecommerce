89237f0fcf3a3764c5842cb7eb168a6c7e21112c
lilchix <104845593+lilchix@users.noreply.github.com>
2023-09-23 23:36:06 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,155 +1,169 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <!-- Metatag e collegamenti CSS -->
-    <!-- ... (il resto del tuo codice HTML) ... -->
-    <!-- Swiper.js -->
-    <link
-        rel="stylesheet"
-        href="https://unpkg.com/swiper/swiper-bundle.min.css"
-    />
-</head>
-<body>
-    <!-- ... (il resto del tuo codice HTML) ... -->
-    <!-- Inclusione di jQuery -->
-    <script
-        src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+KHjsl5v6r+8xn2p3s/R5D5ZKm9T9f1n2O4Rl5Lpbc9j5V"
-        crossorigin="anonymous"
-    ></script>
-    <!-- Inclusione di Swiper.js -->
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <!-- Il tuo codice JavaScript -->
-    <script>
-        function aggiungiAlCarrello() {
-            var carrello = document.getElementById("carrello");
-            var numero = parseInt(carrello.innerHTML);
-            numero++;
-            carrello.innerHTML = numero;
-        }
-        function rimuoviDalCarrello() {
-            var carrello = document.getElementById("carrello");
-            var numero = parseInt(carrello.innerHTML);
-            numero--;
-            carrello.innerHTML = numero;
-        }
-        function svuotaCarrello() {
-            var carrello = document.getElementById("carrello");
-            carrello.innerHTML = 0;
-        }
+var theToggle = document.getElementById('toggle');
 
-        /* menu */
-        var theToggle = document.getElementById('toggle');
-        var menu = document.getElementById('menu'); // Assicurati di utilizzare l'ID corretto del tuo menu
+// based on Todd Motto functions
+// https://toddmotto.com/labs/reusable-js/
 
-        theToggle.onclick = function() {
-            menu.classList.toggle('fixed-menu'); // Aggiunge o rimuove la classe 'fixed-menu'
-            toggleClass(this, 'on');
-            return false;
+// hasClass
+function hasClass(elem, className) {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+        elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
         }
-
-        // hasClass
-        function hasClass(elem, className) {
-            return elem.classList.contains(className);
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
         }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
 
-        // Funzione per aggiungere una classe
-        function addClass(elem, className) {
-            if (!hasClass(elem, className)) {
-                elem.classList.add(className);
-            }
-        }
+theToggle.onclick = function() {
+    toggleClass(this, 'on');
+    return false;
+}
 
-        // Funzione per rimuovere una classe
-        function removeClass(elem, className) {
-            elem.classList.remove(className);
-        }
+function aggiungiAlCarrello() {
+    var carrello = document.getElementById("carrello");
+    var numero = parseInt(carrello.innerHTML);
+    numero++;
+    carrello.innerHTML = numero;
+}
+function rimuoviDalCarrello() {
+    var carrello = document.getElementById("carrello");
+    var numero = parseInt(carrello.innerHTML);
+    numero--;
+    carrello.innerHTML = numero;
+}
+function svuotaCarrello() {
+    var carrello = document.getElementById("carrello");
+    carrello.innerHTML = 0;
+}
 
-        // toggleClass
-        function toggleClass(elem, className) {
-            var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
-            if (hasClass(elem, className)) {
-                while (newClass.indexOf(" " + className + " ") >= 0) {
-                    newClass = newClass.replace(" " + className + " ", " ");
+/*menu*/
+var Menu = {
+    el: {
+        menu: $('.menu'),
+        menuTop: $('.menu-top'),
+        menuClose: $('.menu-close'),
+        menuMiddle: $('.menu-middle'),
+        menuBottom: $('.menu-bottom'),
+        menuText: $('.menu-text')
+    },
+
+    init: function() {
+        Menu.bindUIactions();
+    },
+
+    bindUIactions: function() {
+        Menu.el.menu
+            .on(
+                'click',
+                function(event) {
+                    Menu.activateMenu(event);
+                    event.preventDefault();
                 }
-                elem.className = newClass.replace(/^\s+|\s+$/g, '');
-            } else {
-                elem.className += ' ' + className;
-            }
-        }
+            );
+    },
 
-        theToggle.onclick = function () {
-            toggleClass(this, 'on');
-            return false;
-        }
+    activateMenu: function() {
+        Menu.el.menuTop.toggleClass('menu-top-expand expand');
+        Menu.el.menuMiddle.toggleClass('menu-middle-expand expand');
+        Menu.el.menuBottom.toggleClass('menu-bottom-expand expand'); 
+        Menu.el.menuText.toggleClass('menu-text-expand');
+        Menu.el.menuClose.toggleClass('menu-close-visible');
+    }
+};
 
-        /* swiper */
-        var swiper = new Swiper(".mySwiper", {
-            spaceBetween: 30,
-            centeredSlides: true,
-            autoplay: {
-                delay: 2500,
-                disableOnInteraction: false,
-            },
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
+// Stop menu item click closing the menu
+$(".menu .menu-global").click(function(e) {
+    e.stopPropagation();
+});
 
-        /* jQuery ready compatto */
-        $(function () {
-            $("h1").click(function () {
-                $("#prodotto1").slideDown();
-                $("#prodotto2").slideDown("slow");
-                $("#prodotto3").slideDown(3000);
-            });
-        });
+Menu.init();
 
-        // Prodotti
-        var quantity1Input = document.getElementById("quantity1");
-        var quantity2Input = document.getElementById("quantity2");
-        var quantity3Input = document.getElementById("quantity3");
-        var totalCost1Output = document.getElementById("totalCost1");
-        var totalCost2Output = document.getElementById("totalCost2");
-        var totalCost3Output = document.getElementById("totalCost3");
-        var subTotalOutput = document.getElementById("subTotal");
-        var shippingCostOutput = document.getElementById("shippingCost");
-        var grandTotalOutput = document.getElementById("grandTotal");
+/*swiper*/
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
-        quantity1Input.addEventListener("input", updateCosts);
-        quantity2Input.addEventListener("input", updateCosts);
-        quantity3Input.addEventListener("input", updateCosts);
+/*jQuery ready compatto*/
+$(function() {
+    $("h1").click(function() {
+        $("#prodotto1").slideDown();
+        $("#prodotto2").slideDown("slow");
+        $("#prodotto3").slideDown(3000);
+    });
+});
 
-        function updateCosts() {
-            var quantity1 = parseInt(quantity1Input.value);
-            var quantity2 = parseInt(quantity2Input.value);
-            var quantity3 = parseInt(quantity3Input.value);
+// prodotti
+var quantity1Input = document.getElementById("quantity1");
+var quantity2Input = document.getElementById("quantity2");
+var quantity3Input = document.getElementById("quantity3");
+var totalCost1Output = document.getElementById("totalCost1");
+var totalCost2Output = document.getElementById("totalCost2");
+var totalCost3Output = document.getElementById("totalCost3");
+var subTotalOutput = document.getElementById("subTotal");
+var shippingCostOutput = document.getElementById("shippingCost");
+var grandTotalOutput = document.getElementById("grandTotal");
 
-            var cost1 = 20;
-            var cost2 = 13;
-            var cost3 = 15;
+quantity1Input.addEventListener("input", updateCosts);
+quantity2Input.addEventListener("input", updateCosts);
+quantity3Input.addEventListener("input", updateCosts);
 
-            var totalCost1 = quantity1 * cost1;
-            var totalCost2 = quantity2 * cost2;
-            var totalCost3 = quantity3 * cost3;
+function updateCosts() {
+    var quantity1 = parseInt(quantity1Input.value);
+    var quantity2 = parseInt(quantity2Input.value);
+    var quantity3 = parseInt(quantity3Input.value);
 
-            totalCost1Output.textContent = "€" + totalCost1;
-            totalCost2Output.textContent = "€" + totalCost2;
-            totalCost3Output.textContent = "€" + totalCost3;
+    var cost1 = 20;
+    var cost2 = 13;
+    var cost3 = 15;
 
-            var subTotal = totalCost1 + totalCost2 + totalCost3;
-            var shippingCost = 8;
-            var grandTotal = subTotal + shippingCost;
+    var totalCost1 = quantity1 * cost1;
+    var totalCost2 = quantity2 * cost2;
+    var totalCost3 = quantity3 * cost3;
 
-            subTotalOutput.textContent = "€" + subTotal;
-            shippingCostOutput.textContent = "€" + shippingCost;
-            grandTotalOutput.textContent = "€" + grandTotal;
-        }
-    </script>
-</body>
-</html>
+    totalCost1Output.textContent = "€" + totalCost1;
+    totalCost2Output.textContent = "€" + totalCost2;
+    totalCost3Output.textContent = "€" + totalCost3;
+
+    var subTotal = totalCost1 + totalCost2 + totalCost3;
+    var shippingCost = 8;
+    var grandTotal = subTotal + shippingCost;
+
+    subTotalOutput.textContent = "€" + subTotal;
+    shippingCostOutput.textContent = "€" + shippingCost;
+    grandTotalOutput.textContent = "€" + grandTotal;
+}
