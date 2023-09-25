b31bd2540dda67a15c16e766e54c7f61a68eac0b
lilchix <104845593+lilchix@users.noreply.github.com>
2023-09-25 18:11:14 +0200

    Update carrello.js

--- javascript/carrello.js
+++ javascript/carrello.js
@@ -1,3 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    on: {
+        autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+    }
+});
 
 var products = {
 	"moka da 4": {
